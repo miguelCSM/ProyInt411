3779aca1dc82d443db34a6c18e399b04115436a2
MikeDevUcamp <115725749+MikeDevUcamp@users.noreply.github.com>
2023-04-20 19:19:20 -0600

    Plantilla

---
+++ proyInt/proyInt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  proyInt
+//
+//  Created by Miguel Gómez Díaz on 20/04/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
